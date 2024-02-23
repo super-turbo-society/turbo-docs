d15ef8dab318127cae977ce7fccbd335fe4110d9
jozanza <hello@jsavary.com>
2024-02-23 04:30:42 -0500

    add github edit url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl: "https://github.com/super-turbo-society/turbo-docs",
+          editUrl: "https://github.com/super-turbo-society/turbo-docs/tree/main/",
         },
         // blog: {
         //   showReadingTime: true,
