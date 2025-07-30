73094a4c4a072e209284bba2ca2708aaa1258912
jozanza <hello@jsavary.com>
2025-07-30 17:51:15 -0400

    bump version in config

--- vocs.config.ts
+++ vocs.config.ts
@@ -57,7 +57,7 @@ export default defineConfig({
     // },
     // { text: "Blog", link: "/blog" },
     {
-      text: "0.8.0-alpha.12",
+      text: "0.8.0",
       items: [
         {
           text: "Changelog",
